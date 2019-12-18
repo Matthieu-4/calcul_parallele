702fe6f13eac98f166de18edeac59c5f4f32dd6a
LaurentVKLE <vkle@smyslov.pedago.ipb.fr>
2019-12-18 16:55:33 +0100

    Debut de la part 2 : seg fault / memory corruption

---
+++ src/function_2.cpp
@@ -0,0 +1,342 @@
+#include <mpi.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <malloc.h>
+
+#include "function_2.hpp"
+using namespace std;
+
+MPI_Status status;
+int PR = 4;
+int tag = 100;
+double *U, *U0;
+int cond_init;
+double sx,sy,dx,dy,dt,Lx,Ly,D,eps1,t,tf, q;
+int i, me, Np, statinfo, i1, i12, iN, iN2, j_1, jN, Ny, Nx, n, k, kmax;
+
+
+void Charge2(int n,
+            int Np,
+            int me,
+            int* i1,
+            int* iN,
+            double* q2){
+
+  int q = n / Np;
+  int r = n % Np;
+
+  if(r == 0){
+    *i1 = q * me;
+    *iN = q * (me + 1) - 1;
+  }else{
+    if( me < r){
+      *i1 = me * (q + 1);
+      *iN = (me + 1) * (q + 1) - 1;
+    }else{
+      *i1 = me * q + r;
+      *iN = (me + 1) * q + r - 1;
+    }
+  }
+
+  // q est le nombre de lignes connues par le proc
+  *q2 = (*iN) - (*i1) + 1;
+
+  // On travaille sur n = nx*ny éléments
+  *i1 = (*i1) * Nx;
+  *iN = (*iN + 1) * Nx - 1;
+}
+
+void Charge_part_domaine(int n,
+            int Np,
+            int me,
+            int* i1,
+            int* i12,
+            int* iN,
+            int* iN2,
+            int h){
+
+  int q = n / Np;
+  int r = n % Np;
+
+  if(r == 0){
+    *i1 = q * me;
+    *iN = q * (me + 1) - 1;
+  }else{
+    if( me < r){
+      *i1 = me * (q + 1);
+      *iN = (me + 1) * (q + 1) - 1;
+    }else{
+      *i1 = me * q + r;
+      *iN = (me + 1) * q + r - 1;
+    }
+  }
+
+  // q est le nombre de lignes connues par le proc
+  // *q2 = (*iN) - (*i1) + 1;
+
+  // On travaille sur n = nx*ny éléments
+  *i1 = (*i1) * Nx;
+  *iN = (*iN + 1) * Nx - 1;
+
+  if (*i1 == 0){
+    i12 = 0;
+    iN2 = iN + Nx*h;
+  }else if (*iN == Nx*Ny-1){
+    i12 = i1 - Nx*h;
+    iN2 = iN;
+  }else{
+    i12 = i1 - Nx*h;
+    iN2 = iN + Nx*h;
+  }
+}
+
+
+void Init(DataFile* dataFile){
+  int i1, iN, i12, iN2;
+  int h = 2;
+  double q;
+
+  Nx = dataFile->Get_Nx();
+  Ny = dataFile->Get_Ny();
+  Lx = dataFile->Get_Lx();
+  Ly = dataFile->Get_Ly();
+  D = dataFile->Get_D();
+  dt = dataFile->Get_dt();
+  eps1 = dataFile->Get_epsilon();
+  kmax = dataFile->Get_kmax();
+  tf = dataFile->Get_tf();
+  cond_init = dataFile->Get_cond_init();
+
+  dx = Lx/(Nx+1);
+  dy = Ly/(Ny+1);
+  sx = D*dt/(dx*dx);
+  sy = D*dt/(dy*dy);
+
+  Charge_part_domaine(Ny,Np,me,&i1,&i12,&iN,&iN2,h);
+
+  U = (double*) calloc((iN2-i12+1), sizeof(double));
+  U0 = (double*) calloc((iN2-i12+1), sizeof(double));
+  for(i = 0; i < iN2-i12+1; i++){
+    U0[i] = 1.;
+  }
+}
+
+
+
+double f(double x, double y, double t){
+  if(cond_init == 3){
+    return exp(-((x - Lx/2) * (x - Lx/2))) * exp(-((y - Ly/2) * (y - Ly/2))) * cos(t*M_PI/2);
+  }else if(cond_init == 2){
+    return sin(x) + cos(y);
+  }else{
+    return 2 * (y - y * y + x - x * x);
+  }
+}
+
+
+double g(double x, double y, double t){
+  if(cond_init == 2){
+    return sin(x) + cos(y);
+  }else{
+    return 0;
+  }
+}
+
+double h(double x, double y, double t){
+  if(cond_init == 3){
+    return 1.;
+  }else if(cond_init == 2){
+    return sin(x) + cos(y);
+  }else{
+    return 0;
+  }
+}
+
+
+
+void ProdMatVect(double D1[],
+                 double D2_m[],
+                 double D2_p[],
+                 double D3_m[],
+                 double D3_p[],
+                 double x[],
+                 double y[],
+                 int i1,
+                 int iN){
+
+
+
+  int i = 0;
+
+
+
+  if(i1 == 0){
+
+    double* x1 = (double*)malloc(sizeof(double) * (iN + Nx + 1));
+    for(i = 0; i < iN + 1; i++){
+      x1[i] = x[i];
+    }
+
+    double q = 0;
+    MPI_Recv(x1 + iN + 1, Nx, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD, &status);
+
+    MPI_Send(x1 + iN - Nx + 1, Nx, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD);
+
+    y[0] = D1[0] * x1[0] + D2_p[0] * x1[1] + D3_p[0] * x1[Nx];
+    for(i = 1; i < Nx; i++){
+      y[i] = D2_m[i]*x1[i-1]+D1[i]*x1[i]+D2_p[i]*x1[i+1]+D3_p[i]*x1[i+Nx];
+    }
+
+    for(i = Nx; i < iN + 1; i++){
+      y[i] = D3_m[i] * x1[i-Nx] + D2_m[i] * x1[i-1] + D1[i] * x1[i] + D2_p[i] * x1[i+1] + D3_p[i] * x1[i+Nx];
+    }
+
+
+    free(x1);
+
+  } else if (iN == Nx*Ny -1){
+
+    double* x3 = (double*)calloc(sizeof(double),iN + Nx - i1 + 1);
+    for(i = Nx; i < Nx + iN - i1 + 1; i++){
+      x3[i] = x[i-Nx];
+    }
+
+    MPI_Send(x3 + Nx, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD);
+    MPI_Recv(x3, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD, &status);
+
+
+    for(i = Nx; i < iN - i1 + 1; i++){
+      y[i - Nx] = D3_m[i-Nx]*x3[i-Nx]+D2_m[i-Nx]*x3[i-1]+D1[i-Nx]*x3[i]+D2_p[i-Nx]*x3[i+1]+D3_p[i-Nx]*x3[i+Nx];
+    }
+
+    for(i = iN - i1 + 1; i < iN + Nx - i1 ; i++){
+      y[i - Nx] = D3_m[i-Nx]*x3[i-Nx]+D2_m[i-Nx]*x3[i-1]+D1[i-Nx]*x3[i]+D2_p[i-Nx]*x3[i+1];
+    }
+
+    y[iN - i1] = D3_m[iN - i1]*x3[iN - i1]+D2_m[iN - i1]*x3[iN + Nx - i1 - 1]+D1[iN - i1]*x3[iN + Nx - i1];
+
+
+    free(x3);
+
+  } else {
+
+    double* x2 = (double*)calloc(sizeof(double),iN + 2 * Nx - i1 + 1);
+    for(i = Nx; i < Nx + iN - i1 + 1; i++){
+      x2[i] = x[i-Nx];
+    }
+
+    MPI_Send(x2 + Nx, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD);
+
+    MPI_Recv(x2, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD, &status);
+
+
+
+    MPI_Recv(x2 + iN - i1 + 1 + Nx, Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD, &status);
+
+    MPI_Send(x2 + iN - i1 + 1 , Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD);
+
+    for(i = Nx; i < Nx + iN - i1 + 1; i++){
+      y[i - Nx] = D3_m[i-Nx]*x2[i-Nx] + D2_m[i-Nx]*x2[i-1] + D1[i-Nx]*x2[i]+D2_p[i-Nx]*x2[i+1] + D3_p[i-Nx]*x2[i+Nx];
+    }
+
+    free(x2);
+  }
+
+}
+
+double prodscal(const double X[],
+                const double Y[],
+                const int i1,
+                const int iN){
+
+  int i;
+  double result = 0, somme = 0;
+  for (i = 0; i < iN - i1 + 1; i++) {
+    result += X[i] * Y[i];
+  }
+
+  MPI_Allreduce(&result, &somme, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+  return somme;
+}
+
+int Reste(int k, int Nx){
+  int i = (k+1) % Nx;
+  if(i == 0){
+    return Nx  ;
+  }
+  return i;
+}
+
+void grad_conj(double D1[],
+                 double D2_m[],
+                 double D2_p[],
+                 double D3_m[],
+                 double D3_p[],
+                 double x[],
+                 double b[],
+                 int i1,
+                 int iN){
+
+
+
+  int i = 0;
+  double* r = (double*)malloc(sizeof(double) * (iN - i1 + 1));
+  double* p = (double*)malloc(sizeof(double) * (iN - i1 + 1));
+  double* r2 = (double*)malloc(sizeof(double) * (iN - i1 + 1));
+  double* z = (double*)malloc(sizeof(double) * (iN - i1 + 1));
+  double* y = (double*)malloc(sizeof(double) * (iN - i1 + 1));
+
+  for(i = 0; i < iN - i1 + 1; i++)
+    x[i] = 293.0;
+  ProdMatVect(D1,D2_m,D2_p,D3_m,D3_p,x,y,i1,iN);
+
+  for(i = 0; i < iN - i1 + 1; i++){
+
+    r[i] = b[i] - y[i];
+    p[i] = r[i];
+
+  }
+  double beta = sqrt(prodscal(r,r,i1,iN));
+  int k = 0;
+  double alpha = 0, gamma = 0;
+  while (beta > eps1 && k <= kmax){
+    ProdMatVect(D1,D2_m,D2_p,D3_m,D3_p,p,z,i1,iN);
+    alpha = prodscal(r,r,i1,iN)/(prodscal(z,p,i1,iN));
+    for(i = 0; i < iN - i1 + 1; i++){
+      x[i] = x[i] + alpha * p[i];
+      r2[i] = r[i];
+      r[i] = r[i] - alpha * z[i];
+    }
+    gamma = prodscal(r,r,i1,iN)/prodscal(r2,r2,i1,iN);
+
+    for(i = 0; i < iN - i1 + 1; i++){
+      p[i] = r[i] + gamma * p[i];
+    }
+    beta = sqrt(prodscal(r2,r2,i1,iN));
+    k++;
+  }
+
+  delete[] r;
+  delete[] p;
+  delete[] r2;
+  delete[] z;
+  delete[] y;
+}
+
+int cmp_vect(const double x[],
+             const double y[],
+             const int Nx,
+             int *test)    // ajouter epsilon (précision)
+{
+  *test = 1;
+  int i = 0;
+  while (i < Nx && fabs(x[i] - y[i]) > 0.0001 )  // epsilon = 000.00001
+    i += 1;
+
+  if ( i == Nx)
+    *test = 0;
+  return *test;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <fstream>
 
-#include "function.hpp"
-#include "matrices.hpp"
+#include "function_2.hpp"
+#include "matrices_2.hpp"
 #include "DataFile.hpp"
 
 using namespace std;
@@ -21,9 +21,7 @@ int main(int argc, char** argv)
     abort();
   }
 
-  DataFile data_file(argv[1]);
-  // Lecture du fichier de données
-  data_file.ReadDataFile();
+
 
 
   int world_rank;
@@ -32,88 +30,247 @@ int main(int argc, char** argv)
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &Np);
   // Get the rank of the process
+  int choice = 0;
+  choice = 1;
+  if (choice == 0){
 
-  double t = 0;
-  Init(&data_file);
-  // !!$  ! On mesure le temps de calcul par proc, premier compteur t1
-  // !!$  call CPU_TIME(t1)
+    DataFile data_file(argv[1]);
+    // Lecture du fichier de données
+    data_file.ReadDataFile();
 
-  // ! Répartition des procs
-  Charge2(Ny,Np,me,&i1,&iN,&q);
-  int nb_per_proc = iN-i1+1;
+    double t = 0;
+    Init(&data_file);
+    // !!$  ! On mesure le temps de calcul par proc, premier compteur t1
+    // !!$  call CPU_TIME(t1)
 
-  double* F = (double*)calloc(sizeof(double), nb_per_proc);
-  double* D1 = (double*)calloc(sizeof(double), nb_per_proc);
-  double* D2_m = (double*)calloc(sizeof(double), nb_per_proc);
-  double* D2_p = (double*)calloc(sizeof(double), nb_per_proc);
-  double* D3_m = (double*)calloc(sizeof(double), nb_per_proc);
-  double* D3_p = (double*)calloc(sizeof(double), nb_per_proc);
+    // ! Répartition des procs
+    Charge2(Ny,Np,me,&i1,&iN,&q);
+    int nb_per_proc = iN-i1+1;
 
-  MatriceDF(D1,D2_m,D2_p,D3_m,D3_p,sx,sy,i1,iN, &data_file);
+    double* F = (double*)calloc(sizeof(double), nb_per_proc);
+    double* D1 = (double*)calloc(sizeof(double), nb_per_proc);
+    double* D2_m = (double*)calloc(sizeof(double), nb_per_proc);
+    double* D2_p = (double*)calloc(sizeof(double), nb_per_proc);
+    double* D3_m = (double*)calloc(sizeof(double), nb_per_proc);
+    double* D3_p = (double*)calloc(sizeof(double), nb_per_proc);
 
-  int k = 0;
-   /////////////////////////// BOUCLE EN TEMPS /////////////////////////
-  while (t<tf)
-  {
-    sec_membre(dx, dy, F,t,i1,iN,&data_file);
-    int toto = 0;
-    for(toto = 0; toto < nb_per_proc; toto++){
-      F[toto] += U0[toto];
-    }
-    grad_conj(D1,D2_m,D2_p,D3_m,D3_p,U, F,i1,iN);
-    for (i = 0; i < nb_per_proc; i++)
-      U0[i] = U[i];
-    t += dt;
-    k += 1;
-    if (k%50 == 0){
-      printf("%d %f\n",k, U0[4]);
-    }
-  }
+    MatriceDF(D1,D2_m,D2_p,D3_m,D3_p,sx,sy,i1,iN, &data_file);
 
-
-  // Ecriture de la solution sur des fichiers .dat pour chaque proc
-
-  ofstream file;
-  ofstream plot;
-
-  char file_name [50];
-  char place_holder [50];
-  char plot_name [50] = "Result/sol.plot";
-  sprintf(file_name, "Result/sol%d.dat", me);
-
-  file.open(file_name, ios::out);
-
-  for(int k = 0; k < iN - i1 + 1; k++){
-    file << Reste(k,Nx)*dx << " " << dy*(1 + (i1 + k)/Nx)  << " " << U[k] << "\n";
-  }
-  file.close();
-
-  int cond_init = data_file.Get_cond_init();
-  if (me == 0)
-  {
-    plot.open(plot_name, ios::out);
-    plot << "set terminal png" << endl;
-    plot << "set output 'Sol.png'" << endl;
-    plot << "set ticslevel 0" << endl;
-    plot << "splot 'Result/sol0.dat'";
-    for (i = 1; i < Np; i++)
+    int k = 0;
+    /////////////////////////// BOUCLE EN TEMPS /////////////////////////
+    while (t<tf)
     {
-      sprintf(place_holder, "'Result/sol%d.dat'", i);
-      plot << ", " << place_holder;
+      sec_membre(dx, dy, F,t,i1,iN,&data_file);
+      int toto = 0;
+      for(toto = 0; toto < nb_per_proc; toto++){
+        F[toto] += U0[toto];
+      }
+      grad_conj(D1,D2_m,D2_p,D3_m,D3_p,U, F,i1,iN);
+      for (i = 0; i < nb_per_proc; i++)
+      U0[i] = U[i];
+      t += dt;
+      k += 1;
+      if (k%50 == 0){
+        printf("%d %f\n",k, U0[4]);
+      }
     }
-    if (cond_init == 1)
+
+
+    // Ecriture de la solution sur des fichiers .dat pour chaque proc
+
+    ofstream file;
+    ofstream plot;
+
+    char file_name [50];
+    char place_holder [50];
+    char plot_name [50] = "Result/sol.plot";
+    sprintf(file_name, "Result/sol%d.dat", me);
+
+    file.open(file_name, ios::out);
+
+    for(int k = 0; k < iN - i1 + 1; k++){
+      file << Reste(k,Nx)*dx << " " << dy*(1 + (i1 + k)/Nx)  << " " << U[k] << "\n";
+    }
+    file.close();
+
+    int cond_init = data_file.Get_cond_init();
+    if (me == 0)
+    {
+      plot.open(plot_name, ios::out);
+      plot << "set terminal png" << endl;
+      plot << "set output 'Sol.png'" << endl;
+      plot << "set ticslevel 0" << endl;
+      plot << "splot 'Result/sol0.dat'";
+      for (i = 1; i < Np; i++)
+      {
+        sprintf(place_holder, "'Result/sol%d.dat'", i);
+        plot << ", " << place_holder;
+      }
+      if (cond_init == 1)
       plot << ", x*(1-x)*y*(1-y)" << endl;
-    else if (cond_init == 2)
+      else if (cond_init == 2)
       plot << ", sin(x)+cos(y)" << endl;
-    plot.close();
+      plot.close();
+    }
+
+    // !!$  ! Deuxième compteur en temps
+    // !!$  call CPU_TIME(t2)
+    // !!$
+    // !!$  ! Temps de calcul final
+    // !!$  temps=t2-t1
+    // !!$  print*,'Le temps de calcul du proc',me,' pour résoudre le problème est :',temps,'s'
   }
 
-  // !!$  ! Deuxième compteur en temps
-  // !!$  call CPU_TIME(t2)
-  // !!$
-  // !!$  ! Temps de calcul final
-  // !!$  temps=t2-t1
-  // !!$  print*,'Le temps de calcul du proc',me,' pour résoudre le problème est :',temps,'s'
+  else if (choice == 1)
+  {
+    int k = 0, l = 0;
+    int i12, iN2;
+
+
+
+    DataFile data_file(argv[1]);
+    // Lecture du fichier de données
+    data_file.ReadDataFile();
+
+    double t = 0;
+    Init(&data_file);
+    // !!$  ! On mesure le temps de calcul par proc, premier compteur t1
+    // !!$  call CPU_TIME(t1)
+
+    int h = 2;
+    // ! Répartition des procs
+    Charge_part_domaine(Ny,Np,me,&i1,&i12,&iN,&iN2,h);
+    int nb_per_proc = iN2-i12+1;
+    printf("0 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+
+    double* F = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* D1 = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* D2_m = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* D2_p = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* D3_m = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* D3_p = (double*)calloc(nb_per_proc, sizeof(double) );
+    double* comp_1 = (double*)calloc(Nx, sizeof(double) );
+    double* comp_2 = (double*)calloc(Nx, sizeof(double) );
+    double* place_holder_1 = (double*)calloc(Nx, sizeof(double) );
+    double* place_holder_2 = (double*)calloc(Nx, sizeof(double) );
+
+    printf("01 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+    MatriceDF(D1,D2_m,D2_p,D3_m,D3_p,sx,sy,i12,iN2, &data_file);
+    printf("02 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+
+    printf("03 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+    // abort();
+    // abort();
+
+
+    /////////////////////////// BOUCLE EN TEMPS /////////////////////////
+    while (t<tf)
+    {
+      printf("1 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+
+      int test1 = 1;
+      int test2 = 1;
+      while (test1 == 1 || test2 == 1)
+      {
+        printf("2 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+
+        sec_membre(dx, dy, F,t,i12,iN2,&data_file);
+        int toto = 0;
+        for(toto = 0; toto < nb_per_proc; toto++){
+          F[toto] += U0[toto];
+        }
+        grad_conj(D1,D2_m,D2_p,D3_m,D3_p,U, F,i12,iN2);
+        for (i = 0; i < nb_per_proc; i++)
+        U0[i] = U[i];
+
+        printf(" 3 me : %d i12 : %d iN2 : %d ; l = %d\n ",me,i12,iN2,l);
+
+        if (i12 == 0){
+          MPI_Recv(comp_2, Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD, &status);
+          MPI_Send(U + iN2 - i12 - Nx*(h + 1) , Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD);
+          test1 = 0;
+          for (l = iN2 - i12 - Nx*(h+1) + 1 ; l < iN2 - i12 - Nx*h ; l++)
+            place_holder_2[l] = U[l];
+          cmp_vect( place_holder_2, comp_2, Nx, &test2);
+
+        }else if (iN2 == Nx*Ny - 1){
+          MPI_Send(U + Nx*(h-1), Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD);
+          MPI_Recv(comp_1, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD, &status);
+          for (l = Nx*h ; l < Nx*(h+1) - 1 ; l++)
+            place_holder_1[l] = U[l];
+          cmp_vect( place_holder_1, comp_1, Nx, &test1);
+          test2 = 0;
+
+        }else{
+          MPI_Send(U + Nx*(h-1), Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD);
+          MPI_Recv(comp_1, Nx, MPI_DOUBLE, me-1, tag, MPI_COMM_WORLD, &status);
+          MPI_Recv(comp_2, Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD, &status);
+          MPI_Send(U + iN2 - i12 - Nx*(h + 1) , Nx, MPI_DOUBLE, me+1, tag, MPI_COMM_WORLD);
+
+          for (l = Nx*h ; l < Nx*(h+1) - 1 ; l++)
+            place_holder_1[l] = U[l];
+          cmp_vect( place_holder_1, comp_1, Nx, &test1);
+
+          for (l = iN2 - i12 - Nx*(h+1) + 1 ; l < iN2 - i12 - Nx*h ; l++)
+            place_holder_2[l] = U[l];
+          cmp_vect( place_holder_2, comp_2, Nx, &test2);
+        }
+
+      }
+      t += dt;
+      k += 1;
+      if (k%50 == 0){
+        printf("%d %f\n",k, U0[4]);
+      }
+    }
+
+
+    // Ecriture de la solution sur des fichiers .dat pour chaque proc
+
+    ofstream file;
+    ofstream plot;
+
+    char file_name [50];
+    char place_holder [50];
+    char plot_name [50] = "Result/sol.plot";
+    sprintf(file_name, "Result/sol%d.dat", me);
+
+    file.open(file_name, ios::out);
+
+    for(int k = 0; k < iN - i1 + 1; k++){
+      file << Reste(k,Nx)*dx << " " << dy*(1 + (i1 + k)/Nx)  << " " << U[k] << "\n";
+    }
+    file.close();
+
+    int cond_init = data_file.Get_cond_init();
+    if (me == 0)
+    {
+      plot.open(plot_name, ios::out);
+      plot << "set terminal png" << endl;
+      plot << "set output 'Sol.png'" << endl;
+      plot << "set ticslevel 0" << endl;
+      plot << "splot 'Result/sol0.dat'";
+      for (i = 1; i < Np; i++)
+      {
+        sprintf(place_holder, "'Result/sol%d.dat'", i);
+        plot << ", " << place_holder;
+      }
+      if (cond_init == 1)
+      plot << ", x*(1-x)*y*(1-y)" << endl;
+      else if (cond_init == 2)
+      plot << ", sin(x)+cos(y)" << endl;
+      plot.close();
+    }
+
+    // !!$  ! Deuxième compteur en temps
+    // !!$  call CPU_TIME(t2)
+    // !!$
+    // !!$  ! Temps de calcul final
+    // !!$  temps=t2-t1
+    // !!$  print*,'Le temps de calcul du proc',me,' pour résoudre le problème est :',temps,'s'
+
+
+  }
 
   MPI_Finalize();
 
